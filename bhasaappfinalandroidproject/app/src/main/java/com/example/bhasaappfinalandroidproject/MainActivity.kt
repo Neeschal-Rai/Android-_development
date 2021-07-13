65139f37a5d2da9d163183ffdfc039355e53153f
Neeschal Rai <56905212+Neeschal-Rai@users.noreply.github.com>
2021-07-13 10:15:53 +0545

    second commit

--- bhasaappfinalandroidproject/app/src/main/java/com/example/bhasaappfinalandroidproject/MainActivity.kt
+++ bhasaappfinalandroidproject/app/src/main/java/com/example/bhasaappfinalandroidproject/MainActivity.kt
@@ -2,8 +2,17 @@ package com.example.bhasaappfinalandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edUsername : EditText
+    private lateinit var edPassword : EditText
+    private lateinit var btnLogin : Button
+    private lateinit var btnForgot : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
